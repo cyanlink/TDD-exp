c889b38eb218823ebd582cb77f4b6d9551bf6c22
linkft <tgvita1999@gmail.com>
2020-06-06 14:57:24 +0800

    unittest

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,39 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    
+    def tearDown(self):
+        self.browser.quit()
 
-assert 'Django' in browser.title
+    def test_can_start_a_list_and_retrieve_it_later(self):
+
+# user goes to our todo list site
+        self.browser.get('http://localhost:8000')
+
+# user noticed the page title
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the Test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
+# user enters a todo item
+
+# user input Buy peacock feathers into textbox
+
+# user hits enter then page updates, page shows peacok feather todo list entry
+
+# still a text box to add another item, usr enter
+
+# page update
+
+# generated unique URL permalink for the list
+
+# user revisit the permalink and see the same todo list again
+
+browser.quit()
+
