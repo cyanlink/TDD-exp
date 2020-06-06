b80b977b81f4760f21acad65b623ff5dbea51389
linkft <tgvita1999@gmail.com>
2020-06-06 21:48:57 +0800

    refactor first steps

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -11,9 +11,8 @@ def home_page(request):
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/lists/unique-list-identifier/')
 
-    items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'home.html')
 
 def view_list(request):
     items = Item.objects.all()
-    return render(request, 'home.html', {'items':items})
+    return render(request, 'list.html', {'items':items})
