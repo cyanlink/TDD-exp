77ea5de5e7dda3191dc05f943fef9844524b274d
linkft <tgvita1999@gmail.com>
2020-06-06 17:30:30 +0800

    multi entry

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest
 from django.http import HttpResponse
 from lists.models import Item
@@ -6,9 +6,7 @@ from lists.models import Item
 # Create your views here.
 def home_page(request):
     if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-    else:
-        new_item_text = ''
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
 
-    return render(request, 'home.html',{'new_item_text': new_item_text})
+    return render(request, 'home.html')
